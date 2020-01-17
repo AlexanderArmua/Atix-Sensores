10f254c4a4d12218c94ba21703bf9b590ecfc44a
Alexander Abregu <aabregu@HEXACTA.COM>
2020-01-17 17:19:41 -0300

    First COmmit

---
+++ src/main/java/com/atix/AtixApplication.java
@@ -0,0 +1,13 @@
+package com.atix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atix/AtixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
